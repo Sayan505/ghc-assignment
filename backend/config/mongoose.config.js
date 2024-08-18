0b13e1459cec8a01e2d8a8547438d2815e10174d
Sayan Bhattacharjee <bsayan8086@gmail.com>
2024-08-18 23:04:11 +0530

    fix: craft mongodb connection string from env vars

--- backend/config/mongoose.config.js
+++ backend/config/mongoose.config.js
@@ -6,7 +6,7 @@ import logger   from "../utils/logger.util.js";
 function connect_db() {
     logger.info("[trying to connect to db ...]");
 
-    return mongoose.connect(`mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@mongodb:27017`, { dbName: process.env.MONGO_DB_NAME }).then(
+    return mongoose.connect(`mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_DB_HOST}:${process.env.MONGO_DB_PORT}`, { dbName: process.env.MONGO_DB_NAME }).then(
         ()  => {
             logger.info(`[connected to db: \"${process.env.MONGO_DB_NAME}\"]`);
         },
