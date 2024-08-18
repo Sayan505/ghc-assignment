645c29708526793ee090b5e4fd8aee5324860e5e
Sayan Bhattacharjee <bsayan8086@gmail.com>
2024-08-18 19:16:59 +0530

    misc: provide curl scripts for showcasing

---
+++ curl_scripts/trigger_checkout_abandoned_webhook.sh
@@ -0,0 +1,1 @@
+curl -X POST -H "Content-Type: application/json" -d @./abandoned_checkout.json http://localhost:3000/webhook/checkout-abandoned/
---
+++ curl_scripts/trigger_order_placed_webhook.sh
@@ -0,0 +1,1 @@
+curl -X POST -H "Content-Type: application/json" -d @./order_placed.json http://localhost:3000/webhook/order-placed/
