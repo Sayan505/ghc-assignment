9e6364490517eacfc7d65e24a15bd90b9367dfe7
Sayan Bhattacharjee <bsayan8086@gmail.com>
2024-08-19 04:21:16 +0530

    fix: config mongoose connection string

--- backend/config/mongoose.config.js
+++ backend/config/mongoose.config.js
@@ -6,7 +6,8 @@ import logger   from "../utils/logger.util.js";
 function connect_db() {
     logger.info("[trying to connect to db ...]");
 
-    return mongoose.connect(`mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_DB_HOST}:${process.env.MONGO_DB_PORT}`, { dbName: process.env.MONGO_DB_NAME }).then(
+    const mongodb_conn_str = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_DB_HOST}:${process.env.MONGO_DB_PORT}`;
+    return mongoose.connect(mongodb_conn_str, { dbName: process.env.MONGO_DB_NAME }).then(
         ()  => {
             logger.info(`[connected to db: \"${process.env.MONGO_DB_NAME}\"]`);
         },
@@ -28,4 +29,3 @@ function disconnect_db() {
 
 
 export default { connect_db, disconnect_db };
-export const mongoose_instance = mongoose;
