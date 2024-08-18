3c5186b2f9f913e27b4765f9664aada359348aec
Sayan Bhattacharjee <bsayan8086@gmail.com>
2024-08-18 23:02:24 +0530

    feat: implement message_schedule getter and setter

--- backend/config/message_schedule.config.js
+++ backend/config/message_schedule.config.js
@@ -1,4 +1,18 @@
-const message_schedule = [
+const message_schedule_schema_ajv = {
+    type: "array",
+    items: {
+        type: "object",
+        properties: {
+            delay: { type: "integer" },
+            msg: { type: "string" } 
+        },
+        required: ["delay", "msg"],
+        additionalProperties: false
+    }
+};
+
+
+const message_schedule_default = [
     { delay: 30 * 60 * 1000,           msg: "1st reminder, 30mins" },
     { delay: 24 * 60 * 60 * 1000,      msg: "2nd reminder, 1 day"  },
     { delay: 3  * 24 * 60 * 60 * 1000, msg: "3rd reminder, 3 days" }
@@ -12,4 +26,17 @@ const message_schedule_quick = [
 ];
 
 
-export default message_schedule_quick;
+var current_message_schedule = message_schedule_quick;
+
+function get_message_schedule() {
+    return current_message_schedule;
+}
+
+function set_message_schedule(message_schedule) {
+    // TODO: validate
+    current_message_schedule = message_schedule;
+    return 1;
+}
+
+
+export { get_message_schedule, set_message_schedule, message_schedule_schema_ajv };
